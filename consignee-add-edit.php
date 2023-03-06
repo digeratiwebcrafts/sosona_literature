06e4aecf7fa7a747a0042fe150884351f0195850
sayanti-manna <125458140+Abubakr353@users.noreply.github.com>
2023-03-06 12:14:22 +0530

    Update consignee-add-edit.php

--- consignee-add-edit.php
+++ consignee-add-edit.php
@@ -78,7 +78,7 @@ include "includes/top-navbar.php";
                         ?>
                         <div class="form-group">
                           <label>Entry Type:<span class="text-danger">*</span></label>
-                          <select class="form-control select-search" data-fouc data-placeholder="-Select Area-" name="entry_type" required disabled>
+                          <select class="form-control select-search" data-fouc data-placeholder="-Select Area-" name="entry_type" required >
                                   <option></option>
                               <option value="Area"<?php if($row['entry_type'] == 'Area') echo "selected"; ?>>Area</option>
                               <option value="Group"<?php if($row['entry_type'] == 'Group') echo "selected"; ?>>Group</option>
@@ -89,7 +89,7 @@ include "includes/top-navbar.php";
                       <?php }else{ ?>
                         <div class="form-group">
                           <label>Entry Type:<span class="text-danger">*</span></label>
-                          <select class="form-control select-search" data-fouc data-placeholder="-Select Area-" name="entry_type" required disabled>
+                          <select class="form-control select-search" data-fouc data-placeholder="-Select Area-" name="entry_type" required >
                           
                               <option value="Region"<?php if($row['entry_type'] == 'Region') echo "selected"; ?>>Region</option>
                           </select>
