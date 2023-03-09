705ac89b3562b0d905c50efd07db817435449eb7
sayanti-manna <125458140+Abubakr353@users.noreply.github.com>
2023-03-09 18:39:31 +0530

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -1,117 +1,155 @@
-                 <body style="background:#f5f5f5;">
+<!DOCTYPE html>
+<html>  
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <style>
+  .tb-heading th{
+    font-size:8pt !important;
+    padding:5px 10px !important;
+    border:1px solid #000 !important;
+    
+  }
+  .or-heading th{
+    font-size:9pt !important;
+    padding:5px 10px !important;
+    border:1px solid #000 !important;
+   
+  }
+  ord-table td{
+   font-size:9pt !important;
+   padding:6px 10px !important;
+    border:1px solid #000 !important;
+      
+  }
+  td{
+     font-size:8pt !important;
+     padding:5px 10px !important;
+     border:1px solid #000 !important;
+     
+  }
+ .col-heading{
+  font-size:14pt !important;
+ }
+ table{
+  width:100%;
+ }
+  </style>
+</head>
+
+<body style="background:#f5f5f5;">
                    <div class="container">
                     <div class="row">
-                        <div class="col">
+                        
                             <h5 style="background:#252b36;color: #fff;font-size: 1.125rem;text-align: center;  margin-bottom:0.625rem;font-weight: 400;line-height:1.5715;padding:10px;">Area Account</h5>
-                        </div>
+                        
                     </div>
                     <div class="row">
-                        <div class="col-6" style="width: 50%;">
+
+                        <div class="col-6" style="width: 49%;">
                             <div class="row" style="display: flex;">
-                                <div class="col" style="width:78%;">
-                                    <p><strong>Abu (Area )</strong> Opening Bal., as on <strong>2023-02-22</strong></p>
+                                <div class="col" style="width:70%;display: flex;float: left;">
+                                    <p style="font-size:12px;width:100%;"><strong>Abu (Area )</strong> Opening Bal., as on <strong>09-03-2023</strong></p>
                                 </div>
-                                <div class="col" style="width: 15%;float: right;">
-                                    <p style="color: #fff;background: #26a69a;text-align: center;padding: 5px 7px;border-radius: 5px;"><strong>200000.00</strong></p>
+                                <div class="col" style="width: 30%;float: right;display: flex;">
+                                    <p style="font-size:12px;color: #fff;background: #26a69a;text-align: center;padding: 5px 7px;border-radius: 5px;width:100%;"><strong>200000.00</strong></p>
                                 </div>
                             </div>
                         </div>
-                      
+                     
                     </div>
-                    <div class="row" style="display:flex;">
-                        <div class="col-order" style="width:50%">
-                            <!-- Basic table -->
-                            <div class="card">
+                    <div class="row" >
+                        <div class="col-order" style="width:50%;float:left;display:flex;">
+                           
+                                
+                                    <table class="ord-table">  
+                                        <thead>
+                                            <tr style="background:#c5c5c5;text-align:center;padding:15px 0">
+                                                <th colspan="5" class="col-heading">Orders</th>
+                                            </tr>
+                                        </thead>
+                                        <thead class="or-heading">
+                                            <tr>
+                                                <th>Order Date</th>
+                                                <th>Naws Order Id</th>
+                                                <th>Order Total</th>
+                                                <th>Area Share</th>
+                                                <th>Area Billing</th>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>21-02-2023</td>
+                                            <td>#order-001</td>
+                                            <td>56974.00</td>
+                                            <td>3800.17</td>
+                                            <td>53173.83</td>
+                                        </tr>
+                                        <tr>
+                                            <td>05-02-2023</td>
+                                            <td>#order-002</td>
+                                            <td>56654.00</td>
+                                            <td>3802.17</td>
+                                            <td>53173.83</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                            
+                        </div>
+                        <div class="col-payment" style="width:49%;float:right;display:flex;" >
+                        
                                 
                                     <table class="table">  
                                         <thead>
-                                            <tr style="background:#eaf8fa;text-align:center;">
-                                                <th colspan="5" style="padding:15px;">Orders</th>
+                                            <tr style="background:#c5c5c5;text-align:center;">
+                                                <th colspan="4" class="col-heading">Payments</th>
                                             </tr>
-                                        
-                                        
+                                        </thead>
+                                        <thead class="tb-heading">
                                             <tr>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Order Date</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Naws Order Id</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Order Total</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Area Share</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Area Billing</th>
+                                                <th>Payment Date</th>
+                                                <th>Payment Amount</th>
+                                                <th>Payment Mode</th>
+                                                <th>Payment Ref. No.</th>
+                                                
                                             </tr>
                                         </thead>
                                         <tbody>
                                         <tr>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">21-02-2023</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">#order-001</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">56974.00</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">3800.17</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">53173.83</td>
+                                            <td>21-02-2023</td>
+                                            <td>#order-001</td>
+                                            <td>56974.00</td>
+                                            <td>3800.17</td>
+                                            
                                         </tr>
                                         <tr>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">05-02-2023</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">#order-002</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">56654.00</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">3802.17</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">53173.83</td>
+                                            <td>05-02-2023</td>
+                                            <td>#order-002</td>
+                                            <td>56654.00</td>
+                                            <td>3802.17</td>
+                                            
                                         </tr>
                                         </tbody>
                                     </table>
-                                
-                            </div>
-                            <!-- /basic table -->
-                        </div>
-                        <div class="col-payment" style="width:50%" >
-                            <!-- Basic table -->
-                            <div class="card">
-                                
-                                    <table class="table"> 
-                                        <thead>
-                                            <tr style="background:#eaf8fa;text-align:center;">
-                                                <th colspan="4" style="padding:15px;">Payments</th>
-                                            </tr>
-                                        </thead>
-                                        <thead>
-                                            <tr>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Date</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Amount</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Mode</th>
-                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Ref. No.</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">28-02-2023</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">5000.00</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">Cash Deposit</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">001</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">26-02-2023</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">3250</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">Bank Transfer</td>
-                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">002</td>
-                                            
-                                        </tr>
-                                            
-                                        </tbody>
-                                    </table>
-                                
-                            </div>
-                            <!-- /basic table -->
+                              
                         </div> 
                     </div>
                   <div class="row">
 
-                        <div class="col-6" style="width: 50%;float: right;">
+                        <div class="col-6" style="width: 49%;float: right;">
                             <div class="row" style="display: flex;">
-                                <div class="col" style="width:78%;display: flex;">
-                                    <p><strong>Total Dues</strong> to SOSONA as on <strong>09-03-2023</strong></p>
+                                <div class="col" style="width:70%;display: flex;float: left;">
+                                    <p style="font-size:12px;margin-right:15px;width:100%;"><strong>Total Dues</strong> to SOSONA as on <strong>09-03-2023</strong></p>
                                 </div>
-                                <div class="col" style="width: 15%;float: right;display: flex;">
-                                    <p style="color: #fff;background: #f58646;text-align: center;padding: 5px 7px;border-radius: 5px;"><strong>200000.00</strong></p>
+                                <div class="col" style="width: 30%;float: right;display: flex;">
+                                    <p style="font-size:12px;color: #fff;background: #f58646;text-align: center;padding: 5px 7px;border-radius: 5px;width:100%;"><strong>200000.00</strong></p>
                                 </div>
                             </div>
                         </div>
                      
                     </div>
                 </div>
+            </body>
+            </html>
