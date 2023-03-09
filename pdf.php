d86763c1137635cba25c3c16b01f9a6ab7953fe3
sayanti-manna <125458140+Abubakr353@users.noreply.github.com>
2023-03-09 14:31:10 +0530

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -1,52 +1,117 @@
- <?php
-//Calling all the lib functions and features
-//This is the Php Controller Page it will contain all the php related main functions 
-require_once __DIR__ . '/vendor/autoload.php';
-$mpdf = new \Mpdf\Mpdf();
-include("lib/start.php");
-include("lib/config.php");
-include("lib/connect.php");
-include("class/functions.php"); 
-$functions = new functions();
-$functions->validUser();
-$command = isset($_POST['command'])?$_POST['command']:'';
-$flag = isset($_REQUEST['flag'])?$_REQUEST['flag']:'';
-$currtime = date("Y-m-d h:i:s");
-$pageTitle = 'Accounts';
+                 <body style="background:#f5f5f5;">
+                   <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <h5 style="background:#252b36;color: #fff;font-size: 1.125rem;text-align: center;  margin-bottom:0.625rem;font-weight: 400;line-height:1.5715;padding:10px;">Area Account</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6" style="width: 50%;">
+                            <div class="row" style="display: flex;">
+                                <div class="col" style="width:78%;">
+                                    <p><strong>Abu (Area )</strong> Opening Bal., as on <strong>2023-02-22</strong></p>
+                                </div>
+                                <div class="col" style="width: 15%;float: right;">
+                                    <p style="color: #fff;background: #26a69a;text-align: center;padding: 5px 7px;border-radius: 5px;"><strong>200000.00</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                      
+                    </div>
+                    <div class="row" style="display:flex;">
+                        <div class="col-order" style="width:50%">
+                            <!-- Basic table -->
+                            <div class="card">
+                                
+                                    <table class="table">  
+                                        <thead>
+                                            <tr style="background:#eaf8fa;text-align:center;">
+                                                <th colspan="5" style="padding:15px;">Orders</th>
+                                            </tr>
+                                        
+                                        
+                                            <tr>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Order Date</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Naws Order Id</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Order Total</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Area Share</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Area Billing</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">21-02-2023</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">#order-001</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">56974.00</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">3800.17</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">53173.83</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">05-02-2023</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">#order-002</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">56654.00</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">3802.17</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">53173.83</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                
+                            </div>
+                            <!-- /basic table -->
+                        </div>
+                        <div class="col-payment" style="width:50%" >
+                            <!-- Basic table -->
+                            <div class="card">
+                                
+                                    <table class="table"> 
+                                        <thead>
+                                            <tr style="background:#eaf8fa;text-align:center;">
+                                                <th colspan="4" style="padding:15px;">Payments</th>
+                                            </tr>
+                                        </thead>
+                                        <thead>
+                                            <tr>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Date</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Amount</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Mode</th>
+                                                <th style="padding:12px 20px;border-bottom:1px solid #000;">Payment Ref. No.</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">28-02-2023</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">5000.00</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">Cash Deposit</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">001</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">26-02-2023</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">3250</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">Bank Transfer</td>
+                                            <td style="padding:12px 20px;border-bottom:1px solid #000;">002</td>
+                                            
+                                        </tr>
+                                            
+                                        </tbody>
+                                    </table>
+                                
+                            </div>
+                            <!-- /basic table -->
+                        </div> 
+                    </div>
+                  <div class="row">
 
-if($command == '1')
-{
-	
-	$filter_name =$_POST['filter_name']? $_POST['filter_name'] : '';
-	
-	
-	
-	if(!empty($filter_name))
-	{
-		if($filter_name == 'All')
-		{
-			$_SESSION['filter_name']= '';
-		}
-		/*else
-		{
-			$_SESSION['filter_name']=$filter_name;
-		}*/
-			
-	}
-	/*else
-	{
-		$_SESSION['filter_name']= '';
-	}*/
-}
-
-/*if($flag == 'reset')
-{
-	unset($_SESSION['filter_name']);
-	header("location:accounts.php");
-	exit;
-}*/
-$filterType = $_SESSION['filter_name'];
-$consigneeLists = $functions->consigneeLists($filterType,'N');
-/// Includes the html Body
-include_once('templates/pdfBody.php');
-?>
+                        <div class="col-6" style="width: 50%;float: right;">
+                            <div class="row" style="display: flex;">
+                                <div class="col" style="width:78%;display: flex;">
+                                    <p><strong>Total Dues</strong> to SOSONA as on <strong>09-03-2023</strong></p>
+                                </div>
+                                <div class="col" style="width: 15%;float: right;display: flex;">
+                                    <p style="color: #fff;background: #f58646;text-align: center;padding: 5px 7px;border-radius: 5px;"><strong>200000.00</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                     
+                    </div>
+                </div>
