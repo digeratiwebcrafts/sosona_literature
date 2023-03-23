c2c613fecdba43f696fc92d00115e280b367863c
amit <amit@digeratiwebcrafts.com>
2023-03-23 16:33:10 +0530

    Update reset-password-recovery-do.php

--- controllers/reset-password-recovery-do.php
+++ controllers/reset-password-recovery-do.php
@@ -25,7 +25,7 @@ if(isset($_POST["submit"])){
       //echo "Error: " . $upd . "<br>" . $con->error;
       $_SESSION['status'] = "error";
       $_SESSION['status_msg'] = "Something is wrong.";
-      header("Location: ../change-password.php");
+      header("Location: ../reset-password-recovery.php");
     }
 
 }
